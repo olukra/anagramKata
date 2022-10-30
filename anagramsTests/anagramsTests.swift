123c35dff0b140e4cd85ca5c6457b25a48bb68fe
oleh <olehsfo@gmail.com>
2022-10-30 10:58:47 -0700

    Test rots returned sort

--- anagramsTests/anagramsTests.swift
+++ anagramsTests/anagramsTests.swift
@@ -7,7 +7,10 @@
 
 import XCTest
 @testable import anagrams
-
+func anagram(word: String)->[String] {
+    
+    return ["sort"]
+}
 final class anagramsTests: XCTestCase {
 
     override func setUpWithError() throws {
@@ -18,14 +21,10 @@ final class anagramsTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    func testRotsShouldReturnSort() {
+        XCTAssertEqual(["sort"], anagram(word: "rots"))
     }
-
     
+
 
 }
