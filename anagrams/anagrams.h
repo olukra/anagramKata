7db75c9c274990b795e1d4583f752ca7876b7510
oleh <olehsfo@gmail.com>
2022-10-30 10:40:10 -0700

    Initial Commit

---
+++ anagrams/anagrams.h
@@ -0,0 +1,18 @@
+//
+//  anagrams.h
+//  anagrams
+//
+//  Created by Oleg B on 10/30/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for anagrams.
+FOUNDATION_EXPORT double anagramsVersionNumber;
+
+//! Project version string for anagrams.
+FOUNDATION_EXPORT const unsigned char anagramsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <anagrams/PublicHeader.h>
+
+
