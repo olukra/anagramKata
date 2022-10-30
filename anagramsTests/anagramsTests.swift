142206856c5e83f6232ad41746c6baab6db4f50d
oleh <olehsfo@gmail.com>
2022-10-30 11:58:08 -0700

    cleanup code

--- anagramsTests/anagramsTests.swift
+++ anagramsTests/anagramsTests.swift
@@ -9,15 +9,6 @@ import XCTest
 import anagrams
 
 final class anagramsTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
     func testRotsShouldReturnSort() {
         XCTAssertEqual(["sort"], anagram(word: "rots"))
     }
@@ -25,6 +16,7 @@ final class anagramsTests: XCTestCase {
     func testNoSolution() {
         XCTAssertEqual([], anagram(word: "car"))
     }
+
     func testsinksShouldReturnSkins() {
         XCTAssertEqual(["sinks"], anagram(word: "skins"))
     }
